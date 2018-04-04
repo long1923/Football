a1c73d5c09bdf7b5d4700e4543f3e225cdcf1218
long1923 <18811737428@163.com>
2018-04-04 17:51:31 +0800

    添加login画面

---
+++ app/src/main/java/com/llong/football/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.llong.football;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
+
